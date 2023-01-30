6dbda66d19954725ebd794e26d4dad5c557fdfac
olin_tp_ar <universalexperimental@protonmail.com>
2023-01-30 19:31:08 +0100

    makefile and cat class

---
+++ ex00/src/Cat.class.cpp
@@ -0,0 +1,41 @@
+#include "Cat.class.hpp"
+#include "Animal.class.hpp"
+
+void Cat::makeSound(void) const
+{
+	std::cout
+	<< "MEOOWWW" << std::endl;
+	return ;
+}
+
+Cat& Cat::operator=(const Cat &assign)
+{
+	std::cout
+	<< "Cat assign overload" << std::endl;
+	this->type = assign.type;
+	return (*this);
+}
+
+Cat::Cat(Cat const &src) : Animal()
+{
+	std::cout
+	<< "Cat copy constructor" << std::endl;
+	this->type = src.type;
+	return ;
+}
+
+Cat::Cat(void) : Animal()
+{
+	std::cout
+	<< "Cat default constructor" << std::endl;
+	this->type = "Cat";
+	return ;
+}
+
+Cat::~Cat(void)
+{
+	std::cout
+	<< "Cat destructor" << std::endl;
+	return ;
+}
+
