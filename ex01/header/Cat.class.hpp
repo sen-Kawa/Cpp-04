e5be4ca2119dc95f4b40a87dda2118f20f936616
olin_tp_ar <universalexperimental@protonmail.com>
2023-02-02 20:49:59 +0100

    classes

---
+++ ex01/header/Cat.class.hpp
@@ -0,0 +1,24 @@
+#ifndef CAT_CLASS_HPP
+# define CAT_CLASS_HPP
+
+#include <iostream>
+#include "Animal.class.hpp"
+
+class Cat : public Animal
+{
+	private:
+
+
+	public:
+
+		virtual void	makeSound(void) const;
+		
+		Cat&			operator=(Cat const &assign);
+
+		Cat(void);
+		Cat(Cat const &src);
+		~Cat(void);
+};
+
+#endif
+
---
+++ ex01/header/Dog.class.hpp
@@ -0,0 +1,24 @@
+#ifndef DOG_CLASS_HPP
+# define DOG_CLASS_HPP
+
+#include <iostream>
+#include "Animal.class.hpp"
+
+class Dog : public Animal
+{
+	private:
+
+
+	public:
+
+		virtual void	makeSound(void) const;
+		
+		Dog&			operator=(Dog const &assign);
+
+		Dog(void);
+		Dog(Dog const &src);
+		~Dog(void);
+};
+
+#endif
+
